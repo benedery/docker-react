458d9432f1d19c51e22e5222a096a3f86a96a147
Ben Edery <benedry@gmail.com>
2020-01-20 12:17:51 +0200

    change test to suit travis

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hey I Just Uploaded Travis Ci Docker React Nginx Container! 
+         Hey I Just Uploaded Travis Ci Docker React Nginx Container
         </p>
       </header>
     </div>
